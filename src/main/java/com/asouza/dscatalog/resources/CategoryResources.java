4276ead522921193cf2cab6471ba1afa5223cd20
anderson-souz4 <andersondesouzalopes69@gmail.com>
2022-11-29 10:26:32 -0300

    Configurações de conexão com o banco, seeding do bdd, DTO, resource, service e repository de Category

---
+++ src/main/java/com/asouza/dscatalog/resources/CategoryResources.java
@@ -0,0 +1,25 @@
+package com.asouza.dscatalog.resources;
+
+import com.asouza.dscatalog.dto.CategoryDTO;
+import com.asouza.dscatalog.entities.Category;
+import com.asouza.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryResources {
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAllCategories(){
+        List<CategoryDTO> allCategories = categoryService.findAll();
+        return ResponseEntity.ok().body(allCategories);
+    }
+}
---
+++ src/main/java/com/asouza/dscatalog/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.asouza.dscatalog.services;
+
+import com.asouza.dscatalog.dto.CategoryDTO;
+import com.asouza.dscatalog.entities.Category;
+import com.asouza.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CategoryService {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> all = categoryRepository.findAll();
+        return all.stream().map(category -> new CategoryDTO(category)).collect(Collectors.toList());
+
+    }
+}
